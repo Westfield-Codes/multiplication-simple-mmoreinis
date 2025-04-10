a55553db4ea39a4ab59197fa48146aedcd5c6c42
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-04-10 09:12:22 -0400

    Update tables-documentation.js

--- tables-documentation.js
+++ tables-documentation.js
@@ -21,7 +21,7 @@
 
 /* Function setUp()
  * Asks if user wants to keep defaults for low, high, questions. 
- * If not default, prompts user to provide these values
+ * If not default, calls changeVar to prompt for new values
  * @param: none
  * @return: {integer} questions
  */
